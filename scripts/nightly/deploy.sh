826e901f59db87cfae7d86d503540bca13863532
Spiros Delviniotis <spyridon.delviniotis@cern.ch>
2016-11-11 14:56:43 +0100

    scripts: add script for nightly
    Signed-off-by: Spiros Delviniotis <spyridon.delviniotis@cern.ch>

---
+++ scripts/nightly/deploy.sh
@@ -0,0 +1,71 @@
+#!/bin/bash -x
+set +x
+
+export DOCKER_DATA=$(pwd)/DOCKER_DATA/
+
+printf "########## Cleaning workspace ##########\n"
+sudo rm -rf $DOCKER_DATA
+docker rm -f $(docker ps -aq) || true
+docker rmi $(docker images -q) || true
+
+printf "########## Copping records and config to workspace ##########\n"
+cp -r ~/dumps ./inspire-next
+cp ~/inspirehep.cfg .
+
+set -e
+
+printf "########## Seting up environment ##########\n"
+pushd $WORKSPACE
+
+pushd inspire-next
+
+printf "########## Clear \'gosts!!!\' containers ##########\n"
+docker-compose kill
+docker-compose rm -f
+
+mkdir -p ${DOCKER_DATA}
+
+printf "########## Pull and set up Docker ##########\n"
+docker-compose pull
+docker-compose -f docker-compose.deps.yml run --rm pip
+docker-compose -f docker-compose.deps.yml run --rm assets
+
+popd
+sudo cp inspirehep.cfg ${DOCKER_DATA}/tmp/virtualenv/var/inspirehep-instance/
+pushd inspire-next
+
+printf "STARTING DAEMON... \n"
+docker-compose up -d
+sleep 5
+echo "	[OK]"
+
+printf "SCALING WORKERS... \n"
+docker-compose runscale worker=3
+sleep 5
+echo "	[OK]"
+
+printf "########## Run Docker ##########\n"
+docker-compose run --rm web inspirehep db create
+docker-compose run --rm web inspirehep index init
+docker-compose run --rm web inspirehep fixtures init
+
+# Demo records first
+#docker-compose run --rm web inspirehep migrator populate -f inspirehep/demosite/data/demo-records.xml.gz --wait=true
+printf "########## Migrating records ##########\n"
+CNT=0
+for file in $(find dumps -type f); do
+    echo "migrating: $file"
+    let 'CNT+=1'
+    if [ $((CNT % 50)) -eq 0 ]; then
+      docker-compose rrunestart indexer
+    fi
+    docker-composerun run --rm web inspirehep migrator populate -f ${file} --wait=true || true
+    echo "migrating: $file 	[OK]" 
+	printf "\n"
+done
+
+printf "########## Count citations ##########\n"
+docker-compose run --rm web inspirehep migrator count_citations
+
+popd
+popd
